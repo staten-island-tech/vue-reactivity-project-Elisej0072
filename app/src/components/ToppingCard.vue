516935b8b930eff5407098aa8335ab5e4ec17d36
Elisej0072 <Elise22x08@gmail.com>
2026-02-12 12:34:05 -0500

    3

---
+++ app/src/components/ToppingCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+
+<div>
+<h2> {{ topping.name }}</h2>
+<h3>{{ topping.price }}</h3>
+</div>
+
+    </div>
+</template>
+
+<script>
+   defineProps({
+        topping:{
+            type: Object,
+            required: true
+        },
+    })
+</script>
+
+<style scoped>
+div{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: 300px;
+    width: 200px;
+    border: 3px solid black
+}
+</style>
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+ <img src = "">
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+const toppings =ref([
+{ name: 'sliced beef', price: ''},
+{ name: 'chicken', price: ''},
+{ name: 'beef balls', price: ''},
+{ name: 'tripe', price: ''},
+{ name: 'beansprouts', price: ''},
+{ name: 'basil', price: ''},
+{ name: 'mint', price: ''},
+{ name: 'onions', price: ''},
+{ name: 'lime', price: ''},
+{ name: 'cilantro', price: ''},
+{ name: 'jalapenos', price: ''},
+])
+
+</script>
+
+<style scoped>
+
+</style>
